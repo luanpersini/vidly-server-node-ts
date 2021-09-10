1788f0b36bdc74037fb0aab935dd5112fdb2b141
Luan Persini <luanpersini@hotmail.com>
2021-09-10 15:49:57 -0300

    feat: should Return **Unexpected Error** if something fail while trying to create the new genre

--- src/core/components/genres/user-interface/api.controllers/add-genre/add-genre-controller.ts
+++ src/core/components/genres/user-interface/api.controllers/add-genre/add-genre-controller.ts
@@ -1,43 +1,29 @@
 import { Controller, HttpRequest, HttpResponse, Validation } from '@/common/interfaces'
-import { badRequest, forbidden } from '@/common/helpers/http-helper'
+import { badRequest, forbidden, ok, serverError } from '@/common/helpers/http-helper'
 
 import { AddGenre } from '@genres/usecases/add-genre/add-genre'
 import { GenreExistsError } from '@/common/errors/genre-exists-error'
 
-// import { AddGenre } from '@components/genres/usecases/add-genre/add-genre'
-
 export class AddGenreController implements Controller {
-  constructor(private readonly validation: Validation, private readonly addGenre: AddGenre) {}
+  constructor(
+    private readonly validation: Validation, 
+    private readonly addGenre: AddGenre
+    ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    // ## Success
-
-    // 1. - [ ] Receive a **POST** request on route **/api/add-genre**
-    // 2. - [ ] **Validate** the requested data
-    // 3. - [ ] **Check** if there is already an genre with the given name
-    // 4. - [ ] Create the new **genre**
-    // 5. - [ ] Return **Ok** with the created genre data
-    // <br/>
-
-    // ## Exceptions
-
-    // 1. - [ ] Return **NotFound** if the api dosent exist
-    // 2. - [ ] Return **BadRequest** if validation returns an error
-    // 3. - [ ] Return **Forbidden** if there is already an genre with the given name
-    // 4. - [ ] Return **Unexpected Error** if something fail while trying to create the new genre
-
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
-    }
-    const { name } = httpRequest.body
-    const genre = await this.addGenre.add({ name })
-    if (!genre) {
-      return forbidden(new GenreExistsError())
-    }
-    return {
-      statusCode: 200,
-      body: httpRequest.body
+    try {
+      const errorsFound = this.validation.validate(httpRequest.body)
+      if (errorsFound) {
+        return badRequest(errorsFound)
+      }
+      const { name } = httpRequest.body
+      const genre = await this.addGenre.add( name )
+      if (!genre) {
+        return forbidden(new GenreExistsError())
+      }
+      return ok(genre)
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
