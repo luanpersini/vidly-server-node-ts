df4a9bc8172ffb777744a99ca9f0ca542e3f589f
Luan Persini <luanpersini@hotmail.com>
2021-09-09 22:58:54 -0300

    feat: add validations tests

--- src/core/components/genres/user-interface/api.controllers/add-genre/add-genre-controller.ts
+++ src/core/components/genres/user-interface/api.controllers/add-genre/add-genre-controller.ts
@@ -1,22 +1,32 @@
-import { Controller, HttpRequest, HttpResponse } from '@/common/interfaces'
+import { Controller, HttpRequest, HttpResponse, Validation } from '@/common/interfaces'
+
+import { badRequest } from '@/common/helpers/http-helper'
 
 export class AddGenreController implements Controller {
   constructor (
-  
+    private readonly validation: Validation
     ) {}
     
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-// 1. - [] Receive a **POST** request on route **/api/add-genre**
-// 2. - [] **Check** if there is already an genre with the given name
-// 3. - [] Create the new **genre** 
-// 4. - [] Return **200** with the created genre data
+    // 1. - [ ] Receive a **POST** request on route **/api/add-genre**
+    // 2. - [x] **Validate** the requested data
+    // 3. - [ ] **Check** if there is already an genre with the given name
+    // 4. - [ ] Create the new **genre** 
+    // 5. - [ ] Return **200** with the created genre data
 
+//Exceptions
+ // 1. - [ ] Return **404** if the api dosent exist
+// 2. - [x] Return **400** if validation returns an error
+// 3. - [ ] Return **403** if there is already an genre with the given name 
+// 4. - [ ] Return **500** if something fail while trying to create the new genre
+
+const error = this.validation.validate(httpRequest.body)
+if (error) {
+  return badRequest(error)
+}
     return {
     statusCode: 200,
-    body: {
-      answer: 'Answer 555',
-      image: 'http://image-name.com'
-    }
+    body: httpRequest.body
   }
 }
 
