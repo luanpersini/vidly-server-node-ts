f40421fb89d27e2178d57d7ae7b730f72a6c1f4b
Luan Persini <luanpersini@hotmail.com>
2021-09-09 22:56:57 -0300

    feat: add validations tests

--- src/core/components/genres/user-interface/api.controllers/add-genre/add-genre-controller.spec.ts
+++ src/core/components/genres/user-interface/api.controllers/add-genre/add-genre-controller.spec.ts
@@ -1,16 +1,42 @@
+import { HttpRequest, Validation } from '@/common/interfaces'
+
 import { AddGenreController } from './add-genre-controller'
+import { MissingParamError } from '@/common/errors'
+import { badRequest } from '@/common/helpers/http-helper'
+import { mockValidation } from '@/tests/mock-validation'
 
 type SutTypes = {
   sut: AddGenreController 
+  validationStub: Validation
 }
 
 const makeSut = (): SutTypes => {  
-  const sut = new AddGenreController()
+  const validationStub = mockValidation()
+  const sut = new AddGenreController(validationStub)
   return {
-    sut
+    sut,
+    validationStub
   }
 }
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    genre: 'any_genre'
+  }
+})
 
-describe('Add Genre', () => {
-  
+
+describe('AddGenreController', () => {
+  test('Should call Validation with correct value', async () => {
+    const { sut, validationStub } = makeSut()
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+    const httpRequest = makeFakeRequest()
+    await sut.handle(httpRequest)
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+  })
 })
