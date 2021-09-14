8612e940f410072bc716d729ae68fb377c6c63f7
Luan Persini <luanpersini@hotmail.com>
2021-09-14 03:31:51 -0300

    chore: add cors and express.json middlewares

--- src/common/server/config/app.ts
+++ src/common/server/config/app.ts
@@ -1,6 +1,11 @@
+import cors from 'cors'
 import express from 'express'
 import setupRoutes from './routes'
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+//routes last
 setupRoutes(app)
 export default app
