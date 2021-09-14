9b63a452f46abce91a03f2780afdf97c1bbf5619
Luan Persini <luanpersini@hotmail.com>
2021-09-14 03:24:45 -0300

    refactor: add-genre controller

--- src/core/components/genres/user-interface.api.controllers/add-genre/add-genre-controller.ts
+++ src/core/components/genres/user-interface.api.controllers/add-genre/add-genre-controller.ts
@@ -1,26 +1,29 @@
 import { Controller, HttpRequest, HttpResponse, Validation } from '@/common/interfaces'
+import Joi, { ObjectSchema } from 'joi'
 import { badRequest, forbidden, ok, serverError } from '@/common/helpers/http-helper'
 
 import { AddGenre } from '@genres/usecases/add-genre/add-genre'
 import { GenreExistsError } from '@/common/errors/genre-exists-error'
+import { ValidationError } from '@/common/errors/validation-error'
 
 export class AddGenreController implements Controller {
   constructor(
-    private readonly validation: Validation, 
-    private readonly addGenre: AddGenre
-    ) {}
+    private readonly validation: Validation,
+    private readonly addGenre: AddGenre,
+    private readonly validationSchema: ObjectSchema
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    try {
-      const errorsFound = this.validation.validate(httpRequest.body)
-      if (errorsFound) {
-        return badRequest(errorsFound)
+    try {      
+      const error = this.validation.validate(httpRequest.body, this.validationSchema)     
+      if (error) {
+        return badRequest(new ValidationError(error))
       }
-      const { name } = httpRequest.body
-      const genre = await this.addGenre.add( name )
+      const { name } = httpRequest.body      
+      const genre = await this.addGenre.add(name)      
       if (!genre) {
         return forbidden(new GenreExistsError())
-      }
+      }      
       return ok(genre)
     } catch (error) {
       return serverError(error)
---
+++ src/core/components/genres/user-interface.api.controllers/add-genre/infra.factories/add-genre-controller-factory.ts
@@ -0,0 +1,13 @@
+import { AddGenreController } from '../add-genre-controller'
+import { Controller } from '@/common/interfaces'
+import Joi from 'joi'
+import { makeAddGenreUsecase } from '@genres/usecases/add-genre/infra.add-genre-factory'
+import { makeValidation } from '@/common/validation/validate-factory'
+
+const validationSchema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required()
+}) 
+
+export const makeAddGenreController = (): Controller => {
+  return new AddGenreController(makeValidation(), makeAddGenreUsecase(), validationSchema )
+}
