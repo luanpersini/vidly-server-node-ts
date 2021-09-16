127fd690af0b1db532d6e930c6e09d1d5f5e3560
Luan Persini <luanpersini@hotmail.com>
2021-09-16 13:52:43 -0300

    refactor: add genre joi validation schema

--- src/core/components/genres/user-interface.api.controllers/add-genre/add-genre-joi-validation-schema.spec.ts
+++ src/core/components/genres/user-interface.api.controllers/add-genre/add-genre-joi-validation-schema.spec.ts
@@ -1,8 +1,3 @@
-// test('no test', async () => {
-//   const test = 2 + 2
-//   expect(test).toBe(4)
-// })
-
 import Joi from 'joi';
 import { addGenreValidationSchema } from './add-genre-joi-validation-schema';
 import convert from 'joi-to-json'
@@ -10,7 +5,7 @@ import convert from 'joi-to-json'
 const addGenreValidationSchemaConverted = convert(addGenreValidationSchema);
 
 describe('AddGenreController', () => {
-  test('validationSchema should be equal to addGenreValidationSchema', async () => {
+  test('should ensure there will be no unwanted changes to addGenreValidationSchema', async () => {
     const validationSchema = convert(Joi.object({
       name: Joi.string().min(3).max(30).required()
     })) 
--- src/core/components/genres/user-interface.api.controllers/add-genre/add-genre-joi-validation-schema.ts
+++ src/core/components/genres/user-interface.api.controllers/add-genre/add-genre-joi-validation-schema.ts
@@ -1,4 +1,5 @@
 import Joi from 'joi';
+import { MissingParamError } from '@/common/errors';
 
 export const addGenreValidationSchema = Joi.object({
   name: Joi.string().min(3).max(30).required()
