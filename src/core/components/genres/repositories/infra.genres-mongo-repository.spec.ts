2f8b062b3a3ea9c801ee470b94afd5a4b2399582
Luan Persini <luanpersini@hotmail.com>
2021-09-19 22:50:07 -0300

    test: genres-mongo-repository loadAll

--- src/core/components/genres/repositories/infra.genres-mongo-repository.spec.ts
+++ src/core/components/genres/repositories/infra.genres-mongo-repository.spec.ts
@@ -1,7 +1,6 @@
 import { Collection } from 'mongodb'
 import { GenreMongoRepository } from './infra.genres-mongo-repository'
 import { MongoHelper } from '@/infra/persistence/db/helpers/mongo-helper'
-import { mockGenreModel } from '@genres/tests.mocks'
 
 let genreCollection: Collection
 
@@ -15,16 +14,16 @@ describe('Genres Mongo Repository', () => {
   })
 
   beforeEach(async () => {
-    genreCollection = await MongoHelper.getCollection('genres')
-    await genreCollection.deleteMany({})
-    
+   genreCollection = await MongoHelper.getCollection('genres')     
+   await genreCollection.deleteMany({})
   })  
+  
   const makeSut = (): GenreMongoRepository => {
     return new GenreMongoRepository()
   }
   const genre_name = 'any_name'
 
-  describe('open()', () => {
+  describe('add()', () => {
     test('should return a genre on success', async () => {
       const sut = makeSut()
       const result = await sut.add(genre_name)
@@ -33,8 +32,26 @@ describe('Genres Mongo Repository', () => {
       expect(result.name).toEqual(genre_name)
     })
   })
+  describe('loadAll()', () => {
+    test('should return a list of genres on success', async () => {
+      const sut = makeSut()
+      await genreCollection.insertMany([{ name: 'any_name' },{ name: 'other_name' }])
+      const result = await sut.loadAll() 
+      expect(result).toBeTruthy()
+      expect(result.length).toBe(2)
+      expect(result[0].id).toBeTruthy()
+      expect(result[0].name).toBe('any_name')
+      expect(result[1].id).toBeTruthy()
+      expect(result[1].name).toBe('other_name')  
+    })
+    test('should return an empty list if loadAll dont find anything', async () => {
+      const sut = makeSut()
+      const result = await sut.loadAll()
+      expect(result.length).toBe(0)
+    })    
+  })
   describe('loadByName()', () => {
-    test('should return an account on loadByName success', async () => {
+    test('should return an genre on success', async () => {
       const sut = makeSut()
       await genreCollection.insertOne({ name: genre_name })
       const result = await sut.loadByName(genre_name)
