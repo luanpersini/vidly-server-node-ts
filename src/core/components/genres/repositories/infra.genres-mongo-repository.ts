56414b84a6d6c489fd07d948021a519b32a08caf
Luan Persini <luanpersini@hotmail.com>
2021-09-19 22:50:19 -0300

    feat: genres-mongo-repository loadAll

--- src/core/components/genres/repositories/infra.genres-mongo-repository.ts
+++ src/core/components/genres/repositories/infra.genres-mongo-repository.ts
@@ -3,15 +3,18 @@ import { GenresRepository } from './genres-repository'
 import { MongoHelper } from '@/infra/persistence/db/helpers/mongo-helper'
 
 export class GenreMongoRepository implements GenresRepository {
-  constructor(private readonly collection: string = 'genre') {}
+  constructor(
+    private readonly collection: string = 'genres'
+    ) {}
   async add(name: string): Promise<GenreModel> {
     const genreCollection = await MongoHelper.getCollection(this.collection)
     const result = await genreCollection.insertOne({name})
     return MongoHelper.map(result.ops[0])
   }
   async loadAll(): Promise<GenreModel[]> {  
-    // Not Implemented  
-    return null
+    const genreCollection = await MongoHelper.getCollection(this.collection)
+    const result = await genreCollection.find().toArray()
+    return MongoHelper.mapCollection(result) 
   }
   async loadByName(name: string): Promise<GenreModel> {
     const genreCollection = await MongoHelper.getCollection(this.collection)
